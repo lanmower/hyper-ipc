d669df9ba1490d37547179f1009e0510edb21a2f
Moonshine Mclanmower <almagestfraternite@gmail.com>
2021-07-27 12:30:19 +0200

    key operation

--- index.js
+++ index.js
@@ -2,33 +2,47 @@ const DHT = require("@hyperswarm/dht");
 const crypto = require('hypercore-crypto')
 
 const node = new DHT({});
-module.exports = (key='')=>{
-  return {
-    serve: (command, cb)=>{
-      const keyPair = crypto.keyPair(crypto.data(Buffer.from(key+command)));
-      const server = node.createServer();
-      server.on("connection", function(socket) {
-        socket.on("data", async data => {
-          cb(data, (err, output)=>{
-            if(err) throw err;
-            socket.write(output);
-            socket.end();
-            socket.on('error', function(e){throw e});
-          });
-        });
-      });
-      server.listen(keyPair);
-    },
-    run:(command, args, cb)=>{
-      const keyPair = crypto.keyPair(crypto.data(Buffer.from(key+command)));
-      node.connect(keyPair.publicKey);
+
+const runKey = (key, args, cb)=>{
+   return new Promise(resolve=>{
+      node.connect(key);
       const socket = node.connect(keyPair.publicKey); 
       socket.on("data", (res)=>{
         cb(null, res);
         socket.end();
-        socket.on('error', function(e){throw e});
+        resolve(res);
       });
+      socket.on('error', function(e){throw e});
       socket.write(args);
+   })
+}
+
+module.exports = (key='')=>{
+  return {
+    serve: (command, cb)=>{
+      return new Promise(resolve=>{
+        const keyPair = crypto.keyPair(crypto.data(Buffer.from(key+command)));
+        const server = node.createServer();
+        server.on("connection", function(socket) {
+          socket.on('error', function(e){throw e});
+          socket.on("data", async data => {
+            cb(data, (err, output)=>{
+              if(err) throw err;
+              socket.write(output);
+              socket.end();
+              resolve(output);
+            });
+          });
+        });
+        server.listen(keyPair);
+      })
+    },
+    run:(command, args, cb)=>{
+      const keyPair = crypto.keyPair(crypto.data(Buffer.from(key+command)));
+      return runKey(keyPair.publicKey, args, cb)
+    },
+    runKey:(key, args, cb)=>{
+      return runKey(key, args, cb);
     }
   }
 }
