eba2358762cdf15d2465e7bc7fc9cbba35e39283
lanmower <almagestfraternite@gmail.com>
2021-10-14 11:22:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,44 @@
 const DHT = require("@hyperswarm/dht");
 const crypto = require('hypercore-crypto')
-
+const { unpack, pack } = require('msgpackr');
 const node = new DHT({});
 
-const runKey = (key, args, cb)=>{
-   return new Promise(resolve=>{
+const runKey = (key, args)=>{
+   return new Promise((pass, fail)=>{
       const socket = node.connect(key);
       socket.on("data", (res)=>{
-        cb(null, res);
         socket.end();
-        resolve(res);
+        pass(unpack(res));
       });
-      socket.on('error', function(e){throw e});
-      socket.write(args);
+      socket.on('error', error=>fail({error}));
+      console.log(args);
+      socket.write(pack(args));
    })
 }
 
 module.exports = (key='')=>{
   return {
     serve: (command, cb)=>{
-      return new Promise(resolve=>{
-        const keyPair = crypto.keyPair(crypto.data(Buffer.from(key+command)));
-        const server = node.createServer();
-        server.on("connection", function(socket) {
-          socket.on('error', function(e){throw e});
-          socket.on("data", async data => {
-            cb(data, (err, output)=>{
-              if(err) throw err;
-              socket.write(output);
-              socket.end();
-              resolve(output);
-            });
-          });
-        });
-        server.listen(keyPair);
-      })
-    },
-    run:(command, args, cb)=>{
       const keyPair = crypto.keyPair(crypto.data(Buffer.from(key+command)));
-      return runKey(keyPair.publicKey, args, cb)
+      const server = node.createServer();
+      server.on("connection", function(socket) {
+        socket.on('error', function(e){throw e});
+        socket.on("data", async data => {
+          try {
+            socket.write(pack(await cb(unpack(data))));
+          } catch(error) {
+            console.log({error})
+            socket.write(pack({error}));
+          }
+          socket.end();
+        });
+      });
+      server.listen(keyPair);
     },
-    runKey:(key, args, cb)=>{
-      return runKey(key, args, cb);
-    }
+    run:(command, args)=>{
+      const keyPair = crypto.keyPair(crypto.data(Buffer.from(key+command)));
+      return runKey(keyPair.publicKey, args)
+    },
+    runKey
   }
 }
